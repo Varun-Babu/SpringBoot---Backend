8b6c79daffebd0ef12a4dd397e854c452d883f96
Varun-Babu <itsme.varun.26@gmail.com>
2023-01-26 21:57:20 +0530

    initial

---
+++ src/main/java/com/example/hospitalday1/Hospitalday1Application.java
@@ -0,0 +1,13 @@
+package com.example.hospitalday1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hospitalday1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hospitalday1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hospitalday1/Hospitalday1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalday1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hospitalday1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
